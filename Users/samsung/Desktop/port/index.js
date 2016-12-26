c227ce8e6c2cdef4475faa5a22c94af02059c4e6
harshbhardwaj29 <harshbhardwajcsit@gmail.com>
2016-12-27 04:03:06 +0530

    make it better

---
+++ Users/samsung/Desktop/port/index.js
@@ -0,0 +1,15 @@
+var express=require('express');
+var bodyParser=require("body-parser");
+
+var app=express();
+
+app.use(bodyParser.json ());                         //parsing commands use for post data
+app.use(bodyParser.urlencoded({extended:true}));
+
+// // public middleware for front-end
+app.use(express.static("public"));
+
+app.listen(3000,function (req,res) {
+    console.log("server started");
+
+})
